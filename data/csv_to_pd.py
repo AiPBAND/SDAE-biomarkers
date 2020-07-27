da482ebe479b66c6ba45765923e5ee0d0310f88c
Jeremy <jeremy.geo@gmail.com>
2020-07-27 04:26:32 +0000

    add class data

--- data/csv_to_pd.py
+++ data/csv_to_pd.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
 
-FILE_NAME = 'GEO_data_batch_corr_final.csv'
+FILE_NAME = 'GBM_class.csv'
 
 pd.read_csv('csv/'+FILE_NAME, header=0, index_col=0).to_pickle('pd/'+os.path.splitext(FILE_NAME)[0])
