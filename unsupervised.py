4e36e4b2f7a3db714911ed875df202182c9ac000
Jeremy Georges-Filteau <jeremy.geo@gmail.com>
2021-01-08 16:17:31 +0100

    fix all

--- unsupervised.py
+++ unsupervised.py
@@ -19,35 +19,34 @@ parser.add_argument('--dropout', default=[0.1],dest='DROPOUT', type=int, nargs='
 parser.add_argument('--batch', default=15, dest='BATCH_SIZE', type=int, help='Number of samples per batch.')
 parser.add_argument('--epochs', default=150, dest='EPOCHS', type=int, help='Number of epochs.')
 parser.add_argument('--test', default=0.2, dest="TEST_RATIO", type=float, help='Ratio of samples kept out for testing.')
-parser.add_argument('--verbose', default=2, dest="VERBOSITY", type=int, choices=[0,1,2], help='Verbosity level: 0 None, 1 Info, 2 All')
+parser.add_argument('--verbose', default=1, dest="VERBOSITY", type=int, choices=[0,1,2], help='Verbosity level: 0 None, 1 Info, 2 All')
 parser.add_argument('--tolerance', default=5, dest="PATIENCE", type=int, help='Tolenrance to the rate of improvment between each batch. Low values terminate quicker.')
 args = parser.parse_args()
 
-dataframe = pd.read_csv("./data/GEO_features.csv")
+dataframe = pd.read_csv("./data/GEO_features.csv",index_col=0)
 data = dataframe.values
 data = normalize(data)
 
-rs = ShuffleSplit(n_splits=1, test_size=args['TEST_RATIO'], random_state=0)
+rs = ShuffleSplit(n_splits=1, test_size=args.TEST_RATIO, random_state=0)
 split_itterator = rs.split(data)
 i_train, i_test = next(split_itterator)
-train_path = os.path.join("./indices", "train_indices.npy")
-test_path = os.path.join("./indices", "test_indices.npy")
+train_path = os.path.join("./out", "train_indices.npy")
+test_path = os.path.join("./out", "test_indices.npy")
 np.save(train_path, i_train)
 np.save(test_path, i_test)
 
 x_train, x_test = data[i_train], data[i_test]
 
-tensorboard_logs = './ts_logs'
-os.mkdir(tensorboard_logs)
+tensorboard_logs = './out/ts_logs'
 
 
 x_train_out, x_test_out = x_train, x_test
-for idx, num_hidden in enumerate(args["N_NODES"]):
+for idx, num_hidden in enumerate(args.N_NODES):
     print("Training layer {} with {} hidden nodes..".format(idx, num_hidden))
     encoder = Autoencoder(x_train_out.shape[1], num_hidden, tensorboard_logs)
 
-    recon_mse = encoder.fit(x_train_out, x_test_out, batch_size=args["BATCH_SIZE"],
-        num_epochs=args["EPOCHS"], verbose=args["VERBOSITY"], patience=args["PATIENCE"])
+    recon_mse = encoder.fit(x_train_out, x_test_out, batch_size=args.BATCH_SIZE,
+        num_epochs=args.EPOCHS, verbose=args.VERBOSITY, patience=args.PATIENCE)
 
     x_train_out = encoder.encoder_model.predict(x_train_out)
     x_test_out = encoder.encoder_model.predict(x_test_out)
@@ -57,7 +56,6 @@ for idx, num_hidden in enumerate(args["N_NODES"]):
 
     metrics.send('recon_mse_train_layer{}'.format(idx), recon_mse[0])
     metrics.send('recon_mse_test_layer{}'.format(idx), recon_mse[1])
-
-    os.mkdir('./encoder_models')
+    
     model_path = os.path.join("encoders", "model_{}_{}".format(idx,num_hidden))
     encoder.encoder_model.save(model_path)
