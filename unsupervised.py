21933378de62e4bd205b2f676d50dede5a5cb25b
Jeremy Georges-Filteau <jeremy.geo@gmail.com>
2021-01-08 16:40:38 +0100

    epochs

--- unsupervised.py
+++ unsupervised.py
@@ -17,7 +17,7 @@ parser.add_argument('--data-path', type=str,
 parser.add_argument('--num-nodes', default=[2000, 1000, 500], dest='N_NODES', metavar='N', type=int, nargs='+', help='Number of nodes in each layer.')
 parser.add_argument('--dropout', default=[0.1],dest='DROPOUT', type=int, nargs='+', help='Number of nodes in each layer.')
 parser.add_argument('--batch', default=15, dest='BATCH_SIZE', type=int, help='Number of samples per batch.')
-parser.add_argument('--epochs', default=150, dest='EPOCHS', type=int, help='Number of epochs.')
+parser.add_argument('--epochs', default=20, dest='EPOCHS', type=int, help='Number of epochs.')
 parser.add_argument('--test', default=0.2, dest="TEST_RATIO", type=float, help='Ratio of samples kept out for testing.')
 parser.add_argument('--verbose', default=1, dest="VERBOSITY", type=int, choices=[0,1,2], help='Verbosity level: 0 None, 1 Info, 2 All')
 parser.add_argument('--tolerance', default=5, dest="PATIENCE", type=int, help='Tolenrance to the rate of improvment between each batch. Low values terminate quicker.')
