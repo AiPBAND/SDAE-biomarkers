00cb6b1f016edc7da32d3d80088fbb5b8b679a84
jgeofil <jeremy.geo@gmal.com>
2021-03-12 14:01:40 +0100

    imports / structure

---
+++ src/models.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+from tensorflow.keras.metrics import MeanSquaredError, BinaryCrossentropy
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.activations import sigmoid
+from datetime import datetime
+import os
+from typing import List
+from comet_ml import Experiment
+
+
+
+class Autoencoder:
+
+    def __init__(self, num_features, num_hidden, output_dir, dropout_rate=0.05,
+        encoder_act='sigmoid', decoder_act='linear'):
+
+        self.output_dir = output_dir
+        self.mse_func = MeanSquaredError()
+
+        self.num_hidden = num_hidden
+        self.num_inputs = num_features
+
+        self.encoder_act = encoder_act
+
+        input_layer = Input(num_features)
+        dropout_layer = Dropout(dropout_rate)
+        dropout_output = dropout_layer(input_layer)
+
+        self.encoder_layer = Dense(units=self.num_hidden, activation=encoder_act)
+        self.encoder_ouput = self.encoder_layer(dropout_output)
+
+        self.decoder_layer = Dense(units=self.num_inputs, activation=decoder_act)
+        self.decoder_ouput = self.decoder_layer(self.encoder_ouput)
+
+        self.autoencoder_model = Model(input_layer, self.decoder_ouput)
+        self.encoder_model = Model(input_layer, self.encoder_ouput)
+
+    def _mse(self, real):
+        recon = self.autoencoder_model.predict(real)
+        return self.mse_func(real, recon).numpy()
+
+    def set_output_path(self):
+        self.tsb = TensorBoard(log_dir=self.output_dir, write_graph=True,
+            update_freq='batch')
+
+    def fit(self, x_train, x_test, batch_size, num_epochs, loss_fn='mse',
+            optimizer='rmsprop', verbose=1, patience=1, cb=None):
+
+        self.set_output_path()
+
+        early_stop = EarlyStopping(monitor='val_loss', patience=patience, verbose=verbose)
+
+        self.autoencoder_model.compile(loss=loss_fn, optimizer=optimizer)
+
+        cb=Experiment.get_keras_callback()
+        self.autoencoder_model.fit(x_train, x_train,
+            callbacks=[early_stop, self.tsb, cb,], epochs=num_epochs,
+            batch_size=batch_size, validation_data=(x_test, x_test))
+
+        name = "autoencoder-{}".format(self.num_hidden)
+
+        save_path = os.path.join(self.output_dir, name)
+
+        self.autoencoder_model.save(save_path)
+        print("Trained model saved at: {}".format(save_path))
+
+        return self._mse(x_train), self._mse(x_test)
+
+class EncoderStack:
+
+    def __init__(self, autoencoders: List[Autoencoder], output_dir, num_classes=2,
+        dropout_rate=0.05, activation='sigmoid'):
+
+        self.output_dir = output_dir
+        self.bce_func = BinaryCrossentropy()
+
+        model = Sequential()
+        model.add(Input(shape=autoencoders[0].num_inputs))
+
+        for ae in autoencoders:
+            if dropout_rate:
+                model.add(Dropout(dropout_rate))
+            dense = Dense(ae.num_hidden, activation=ae.encoder_act)
+
+            model.add(dense)
+            dense.set_weights(ae.encoder_layer.get_weights())
+
+        model.add(Dense(num_classes, activation=activation))
+        self.model = model
+
+    def set_output_path(self):
+        now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.output_path = os.path.join(self.output_dir, now)
+        self.log_path = os.path.join(self.output_path, "log")
+        self.tsb = TensorBoard(log_dir=self.log_path, write_graph=True,
+            update_freq='batch')
+
+    def _bce(self, x, y):
+        pred = self.model.predict(x)
+        return self.bce_func(y, pred).numpy()
+
+    def fit(self, x_train, y_train, x_test, y_test, batch_size, num_epochs,
+        loss_fn='mse', optimizer='rmsprop', verbose=1):
+
+        self.set_output_path()
+
+        self.model.compile(loss=loss_fn, optimizer=optimizer)
+
+        early_stop = EarlyStopping(monitor='val_loss', patience=1, verbose=2)
+
+        self.model.fit(x_train, y_train, callbacks=[early_stop, self.tsb],
+            epochs=num_epochs, batch_size=batch_size, validation_data=(x_test, y_test))
+
+
+        return self._bce(x_train, y_train), self._bce(x_test, y_test)
--- supervised/task.py
+++ supervised/task.py
@@ -1,132 +1,117 @@
-# Import comet_ml at the top of your file
-from comet_ml import Experiment
+# %%
+"""
+# supervised training of the encoder layers
+"""
 
+# %%
+"""
+## Initial setup
+### Imports
+"""
+
+# %%
 import tensorflow as tf
-import argparse
+print(tf.__version__)
 import pandas as pd
 import numpy as np
-from models import Autoencoder
+from ../src/models import EncoderStack
+from tensorflow.keras.layers.experimental.preprocessing import Normalization
+from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import ShuffleSplit
 from sklearn.preprocessing import normalize
+import dataset
+import neptune
+import neptune_tensorboard as neptune_tb
 import os
-from config import config
+import logging
+
+# %%
 
 
+# %%
+"""
+### Set experiment configuration
+"""
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "--data-path",
-    type=str,
-    dest="data_path",
-    help="Path to the CSV input data. First row headers. First column IDs",
-    default="/data/GEO_features.csv",
-)
-
-parser.add_argument(
-    "--num-nodes",
-    default=[2000, 1000, 500],
-    dest="N_NODES",
-    metavar="N",
-    type=int,
-    nargs="+",
-    help="Number of nodes in each layer.",
-)
-parser.add_argument(
-    "--dropout",
-    default=[0.1],
-    dest="DROPOUT",
-    type=int,
-    nargs="+",
-    help="Number of nodes in each layer.",
-)
-parser.add_argument(
-    "--batch",
-    default=5,
-    dest="BATCH_SIZE",
-    type=int,
-    help="Number of samples per batch.",
-)
-parser.add_argument(
-    "--epochs", default=10, dest="EPOCHS", type=int, help="Number of epochs."
-)
-parser.add_argument(
-    "--test",
-    default=0.2,
-    dest="TEST_RATIO",
-    type=float,
-    help="Ratio of samples kept out for testing.",
-)
-parser.add_argument(
-    "--verbose",
-    default=1,
-    dest="VERBOSITY",
-    type=int,
-    choices=[0, 1, 2],
-    help="Verbosity level: 0 None, 1 Info, 2 All",
-)
-parser.add_argument(
-    "--tolerance",
-    default=3,
-    dest="PATIENCE",
-    type=int,
-    help="Tolenrance to the rate of improvment between each batch. Low values terminate quicker.",
-)
-args = parser.parse_args()
-
-# Report multiple hyperparameters using a dictionary:
-hyper_params = {
-    "num-nodes": args.N_NODES,
-    "dropout": args.DROPOUT,
-    "batch_size": args.BATCH_SIZE,
-    "epochs": args.EPOCHS,
-    "test_ratio": args.TEST_RATIO,
-    "tolerance": args.PATIENCE,
+# %%
+config = {
+    "N_NODES": [1000, 500, 100],
+    "DROPOUT": [0.1],
+    "BATCH_SIZE": 15,
+    "EPOCHS": 5,
+    "TEST_RATIO": 0.30,
+    "DATA_BUCKET": "sdae-geo",
+    "DATA_OBJECT": "GEO_data_batch_corr_final.csv",
+    "DATA_LABELS": " GBM_class.csv",
+    "VERBOSITY": 2,
+    "LOG_DIR": "./log_dir",
+    "PATIENCE":3
 }
 
-dataframe = pd.read_csv("./data/GEO_features.csv", index_col=0)
-data = dataframe.values
-data = normalize(data)
 
-rs = ShuffleSplit(n_splits=1, test_size=args.TEST_RATIO, random_state=0)
-split_itterator = rs.split(data)
-i_train, i_test = next(split_itterator)
-train_path = os.path.join("./out", "train_indices.npy")
-test_path = os.path.join("./out", "test_indices.npy")
-np.save(train_path, i_train)
-np.save(test_path, i_test)
+# %%
+"""
+## Initialize Netptune and Tensorboard logging
+"""
 
-x_train, x_test = data[i_train], data[i_test]
+# %%
+os.environ['NEPTUNE_API_TOKEN']="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiYjNiYmZhYjEtNzc3ZS00Y2NhLWI5NTgtYWU0MmQyMWJhM2I0In0="
+os.environ['NEPTUNE_PROJECT']="jgeof/sdae"
+os.environ['NEPTUNE_NOTEBOOK_ID']="ecd86e96-4da7-44e3-9a17-43da2dfcae35"
+os.environ['NEPTUNE_NOTEBOOK_PATH']="constrained-SDAE/sdae.ipynb"
 
-tensorboard_logs = "./out/ts_logs"
+neptune.init(os.environ['NEPTUNE_PROJECT'], api_token=os.environ['NEPTUNE_API_TOKEN'])
+
+logger = logging.getLogger("SDAE")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+experiment = neptune.create_experiment(name='configuration', params=config, logger=logger)
+
+os.environ['EXP_DIR'] = os.path.join(config["LOG_DIR"], experiment.id)
+os.mkdir(os.environ['EXP_DIR'])
+
+logger.info("project directory: {}".format(os.environ['EXP_DIR']))
+!neptune tensorboard ${EXP_DIR} --project ${NEPTUNE_PROJECT}
+%load_ext tensorboard
+
+# %%
+"""
+### Start tensorboard server
+Tensorboard by running the following command in a terminal:
+"""
+
+# %%
+print("tensorboard --logdir {} --bind_all".format(os.environ['EXP_DIR']))
+
+# %%
+"""
+**Tensorboard cannot server over HTTPS, use external HTTP url: http://34.77.45.86:6006/**
+"""
+
+# %%
+"""
+# Load and preprocess the data
+"""
+
+# %%
+"""
+## Load data from Google Storage
+"""
+
+# %%
+dataframe = dataset.load_gs_data(config['DATA_BUCKET'], config['DATA_OBJECT'], os.environ['EXP_DIR'])
+
+# %%
+classes = pd.read_csv('data/pd/class.csv', header=None, index_col=0).values
+classes = to_categorical(classes)
 
 
-x_train_out, x_test_out = x_train, x_test
-for idx, num_hidden in enumerate(args.N_NODES):
-    experiment = Experiment(**config)
-    experiment.log_parameters(hyper_params)
+model = EncoderStack(encoder_models, 'output/')
 
-    with experiment.train():
-        print("Training layer {} with {} hidden nodes..".format(idx, num_hidden))
-        encoder = Autoencoder(x_train_out.shape[1], num_hidden, tensorboard_logs)
+print("\n##################################################################")
+print("Training layer {} with {} hidden nodes..\n".format(idx, num_hidden))
+loss_train, loss_test = model.fit(x_train, y_train, x_test, y_test, batch_size=BATCH_SIZE, num_epochs=EPOCHS)
 
-        recon_mse = encoder.fit(
-            x_train_out,
-            x_test_out,
-            batch_size=args.BATCH_SIZE,
-            num_epochs=args.EPOCHS,
-            verbose=args.VERBOSITY,
-            patience=args.PATIENCE,
-        )
-
-    with experiment.test():
-        x_train_out = encoder.encoder_model.predict(x_train_out)
-        x_test_out = encoder.encoder_model.predict(x_test_out)
-
-        print("Training losss for layer {}: {} ".format(idx, recon_mse[0]))
-        print("Testing loss for layer {}: {} ".format(idx, recon_mse[1]))
-
-        experiment.log_metrics({"trained_layer_mse": recon_mse[0], "test_layer_mse": recon_mse[1]})
-
-    model_path = os.path.join("encoders", "model_{}_{}".format(idx, num_hidden))
-    encoder.encoder_model.save(model_path)
+print("\nTraining losss: ", loss_train)
+print("\nTesting loss: ", loss_test)
